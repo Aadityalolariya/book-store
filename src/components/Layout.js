38329c8b1bbe0f09e8cf81249b6ee2972f3b2adb
Devmuliya16 <muliyadev16@gmail.com>
2023-08-02 18:37:09 +0530

    created navbar login and signup page

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-
+import  Navbar from './Navbar/Navbar'
 export default function Layout() {
   return (
     <>
-        <div>This is nav</div>
+        <Navbar/>
         <Outlet/>
         <div>This is footer</div>
     </>
